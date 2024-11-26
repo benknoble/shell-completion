f455417018c045d48d00d043fb5fca1c35239768
D. Ben Knoble <ben.knoble+github@gmail.com>
2024-11-26 15:58:16 -0500

    todo: note a problem with top-level collections

--- list-collects.rkt
+++ list-collects.rkt
@@ -22,7 +22,26 @@
                               (current-library-collection-paths))])
          (add-directory-collections c s))]
       [(path? l)
+       ;; TODO: this case generates false-positives for Zsh. For example, say
+       ;; the package named PACK is linked at ~/code/PACK as a "top-level
+       ;; collection" (list "PACK" "~/code/PACK"); then the below code will
+       ;; typically result in ~/code ending up in Zsh's list of prefixes to
+       ;; complete from. But ~/code probably contains more than just Racket
+       ;; packages! In the worst case, a package's info.rkt might specify a
+       ;; different name for the collection than either of PACK or ~/code/PACK
+       ;; (in which case completion can't generate matches) so that the link is,
+       ;; e.g., (list "COLLECT" "~/code/PACK"). Ideally we'd spit out a list of
+       ;; collection names rather than generate a list of directory prefixes for
+       ;; Zsh and then rely on Zsh traversing the filesystem.
+       ;;
+       ;; "Root" collections (list 'root <path>) don't appear to have this
+       ;; problem. See "Collection Links"
        (let ([s (for*/fold ([s s]) ([c (in-list (links #:file l #:root? #f #:with-path? #t))])
+                  ;; TODO: see above. Here we throw away any root collection
+                  ;; names. Because we don't go through
+                  ;; add-directory-collections, either, we end up adding
+                  ;; ~/code/PACK to the list of candidates, which gets turned
+                  ;; into ~/code for Zsh.
                   (hash-set s (cdr c) #t))])
          (for*/fold ([s s]) ([c (in-list (links #:file l #:root? #t))])
            (add-directory-collections c s)))]
